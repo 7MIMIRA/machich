532a689a8ab82f4b2fff8a4095b634904b10334a
7MIMIRA <7mimira@gmail.com>
2021-04-02 00:27:41 -0400

    Implement addURL and getURL controller functions

---
+++ server/controller.js
@@ -0,0 +1,21 @@
+// const Axios = require('axios');
+const URL = require('../model/urls.js');
+const nextKey = require('../utils/incrementKey.js');
+
+const addURL = (url) => {
+  return new Promise((resolve, reject) => {
+    let key = nextKey(URL.getLastKey());
+    URL.add(key, url);
+    resolve(key);
+  })
+}
+
+const getURL = (key) => {
+  return new Promise((resolve, reject) => {
+    let url = URL.get(key);
+    resolve(url);
+  });
+};
+
+module.exports.addURL = addURL;
+module.exports.getURL = getURL;
