590b443acfaaa4e19180ebaacb3ee484da42736f
7MIMIRA <7mimira@gmail.com>
2021-04-02 02:11:07 -0400

    MVP: Build basic UI and implement url shortener feature

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Main() {
+  const [URL, setURL] = useState('');
+  const [newURL, setNewURL] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({ method: 'post', url: '/', data: { url: URL } })
+    .then(result => {
+      setNewURL(result.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+
+  }
+
+  const handleURLChange = (e) => {
+    setURL(e.target.value);
+  }
+
   return (
     <div>
       <h2>URL Shortener</h2>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" onChange={ handleURLChange } value={URL}/>
+        <button type="submit">minify</button>
+      </form>
+      {newURL && <h2>New Link: {newURL}</h2>}
     </div>
   )
 }
