45a064737d2a2826bed1a8f4ae74cee87c37f695
7MIMIRA <7mimira@gmail.com>
2021-04-24 01:11:53 -0400

    Refactor onClick and onChange event handlers
    Combined all onClick events into a single handler.
    Combined all onChange events into a single handler.

--- client/components/Shortener.jsx
+++ client/components/Shortener.jsx
@@ -1,34 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CopyTextField from './CopyTextField.jsx';
 
 function Shortener() {
   const [URL, setURL] = useState('');
   const [newURL, setNewURL] = useState('');
+  const [customKey, setCustomKey] = useState('');
+  const [customURL, setCustomURL] = useState(false);
+  const [enableSubmit, setEnableSubmit] = useState(false);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    if (!customURL) {
+      setEnableSubmit(URL !== '' ? true : false);
+    } else {
+      setEnableSubmit(URL !== '' && customKey !== '' ? true : false);
+    }
+  }, [URL, customURL, customKey]);
+
+  const handleButtonClick = (e, button) => {
     e.preventDefault();
-    axios({ method: 'post', url: '/', data: { url: URL } })
-    .then(result => {
-      setNewURL(result.data);
-    })
-    .catch(err => {
-      console.error(err);
-    })
+    switch (button) {
+      case 'shortenURL':
+        if (URL !== '' && customURL === false || URL !== '' && customKey !== '' && customURL === true) {
+          axios({ method: 'post', url: '/', data: { url: URL, custom: customURL, key: customKey } })
+          .then(result => {
+            setNewURL(result.data);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+        }
+        break;
 
+      case 'swapURLType':
+        setCustomURL(!customURL);
+        setNewURL('');
+        break;
+
+      default:
+        console.error('handleButtonClick invoked without valid button argument');
+        break;
+    }
   }
 
-  const handleURLChange = (e) => {
-    setURL(e.target.value);
+  const handleTextFieldChange = (e, fieldName) => {
+    switch (fieldName) {
+      case 'url':
+        setURL(e.target.value);
+        setNewURL('');
+        break;
+
+      case 'key':
+        setCustomKey(e.target.value);
+        setNewURL('');
+        break;
+
+      default:
+        console.error('handleTextFieldChange invoked without valid fieldName argument');
+        break;
+    }
   }
+
   return (
     <div className="urlShortenerContainer">
-      <p>Enter URL:</p>
-      <form onSubmit={ handleSubmit }>
-        <input type="text" onChange={ handleURLChange } value={URL}/>
-        <button type="submit">shorten</button>
+      {!customURL && <div className="customURLButtonsContainer">
+        <div className="urlTypeButton Selected">Generated</div>
+        <div className="urlTypeButton" onClick={ (e) => {handleButtonClick(e, 'swapURLType')} }>Custom</div>
+      </div>}
+      {customURL && <div className="customURLButtonsContainer">
+        <div className="urlTypeButton" onClick={ (e) => {handleButtonClick(e, 'swapURLType')} }>Generated</div>
+        <div className="urlTypeButton Selected">Custom</div>
+      </div>}
+
+      <form onSubmit={ (e) => {handleButtonClick(e, 'shortenURL')} }>
+        <p className="inputDesc">Target URL</p>
+        <input type="text" placeholder="http://..." onChange={ (e) => {handleTextFieldChange(e, 'url')} } value={ URL }/>
+        {enableSubmit && <button type="submit">shorten</button>}
+        {!enableSubmit && <button disabled type="submit">shorten</button>}
       </form>
-      {newURL && <CopyTextField newURL={newURL} />}
+
+      {customURL && <div className="customKeyContainer">
+        <p className="inputDesc">Custom Path</p>
+        <input className="keyField" type="text" onChange={ (e) => {handleTextFieldChange(e, 'key')} } value={ customKey }/>
+        <p className="customPathExample">{window.location.host}/path</p>
+      </div>}
+
+      {newURL && <CopyTextField newURL={ newURL } />}
     </div>
   )
 }
