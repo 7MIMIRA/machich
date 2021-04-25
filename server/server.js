1c79099096d97346f9c23fcbf9005e065f302b7f
7MIMIRA <7mimira@gmail.com>
2021-04-24 21:54:11 -0400

    Add development environment variables

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const https = require("https");
 const fs = require("fs");
 const express = require('express');
 const app = express();
-
+const keyFilePath = process.env.LOCAL_DEV_KEY || "/etc/letsencrypt/live/machich.app/privkey.pem";
+const certFilePath = process.env.LOCAL_DEV_CERT || "/etc/letsencrypt/live/machich.app/fullchain.pem";
 
 const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/machich.app/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/machich.app/fullchain.pem")
+  key: fs.readFileSync(keyFilePath),
+  cert: fs.readFileSync(certFilePath)
 };
 
 app.use(express.static('public'));
