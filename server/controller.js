d7a2a7d97517e715769bc6c30eb44db461d56911
7MIMIRA <7mimira@gmail.com>
2021-04-25 01:21:19 -0400

    Refactor to increase readability

--- server/controller.js
+++ server/controller.js
@@ -3,35 +3,32 @@ const URL = require('../model/urls.js');
 const nextKey = require('../utils/keyGenerator.js');
 const formatURL = require('../utils/formatURLInput.js');
 
-const addURL = (url) => {
-  return new Promise((resolve, reject) => {
-    let key = nextKey(URL.getLastKey());
-    url = formatURL(url);
-    URL.add(key, url);
-    resolve(key);
-  });
+module.exports = {
+  addURL: (url) => {
+    return new Promise((resolve, reject) => {
+      let key = nextKey(URL.getLastKey());
+      url = formatURL(url);
+      URL.add(key, url);
+      resolve(key);
+    });
+  },
+
+  addCustomURL: (key, url) => {
+    return new Promise((resolve, reject) => {
+      url = formatURL(url);
+      URL.addCustom(key, url);
+      resolve(key);
+    });
+  },
+
+  getURL: (key) => {
+    return new Promise((resolve, reject) => {
+      let url = URL.get(key);
+      resolve(url);
+    });
+  },
+
+  keyAvailable: (key) => {
+    return URL.get(key) === undefined;
+  }
 }
-
-const addCustomURL = (key, url) => {
-  return new Promise((resolve, reject) => {
-    url = formatURL(url);
-    URL.addCustom(key, url);
-    resolve(key);
-  });
-};
-
-const getURL = (key) => {
-  return new Promise((resolve, reject) => {
-    let url = URL.get(key);
-    resolve(url);
-  });
-};
-
-const keyAvailable = (key) => {
-  return URL.get(key) === undefined;
-};
-
-module.exports.addURL = addURL;
-module.exports.addCustomURL = addCustomURL;
-module.exports.getURL = getURL;
-module.exports.keyAvailable = keyAvailable;
