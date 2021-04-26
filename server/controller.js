63f9b1298a0e050005c7ba5a063fef11406c0e1b
7MIMIRA <7mimira@gmail.com>
2021-04-26 02:39:07 -0400

    Refactor to use a database (PostgreSQL) via Sequelize ORM

--- server/controller.js
+++ server/controller.js
@@ -6,29 +6,70 @@ const formatURL = require('../utils/formatURLInput.js');
 module.exports = {
   addURL: (url) => {
     return new Promise((resolve, reject) => {
-      let key = nextKey(URL.getLastKey());
       url = formatURL(url);
-      URL.add(key, url);
-      resolve(key);
+      URL.getLastKey()
+      .then(lastKey => {
+        nextFreeKey(lastKey)
+        .then(newKey => {
+          URL.add(newKey, url)
+          .then(() => {
+            resolve(newKey);
+            URL.setLastKey(newKey);
+          })
+          .catch(err => {
+            reject(err);
+          });
+        })
+        .catch(err => {
+          reject(err);
+        });
+      })
+      .catch(err => {
+        reject(err);
+      });
     });
   },
 
   addCustomURL: (key, url) => {
     return new Promise((resolve, reject) => {
       url = formatURL(url);
-      URL.addCustom(key, url);
-      resolve(key);
+      URL.add(key, url)
+      .then(result => {
+        if (result === undefined)
+          resolve(undefined);
+        resolve(result.dataValues.key);
+      })
+      .catch(err => {
+        reject(err);
+      });
     });
   },
 
   getURL: (key) => {
-    return new Promise((resolve, reject) => {
-      let url = URL.get(key);
-      resolve(url);
+    return URL.get(key)
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      console.error(err);
     });
   },
 
   keyAvailable: (key) => {
-    return URL.get(key) === undefined;
+    return URL.get(key)
+    .then(result => {
+      return result === undefined;
+    })
+    .catch(err => {
+      console.error(err);
+    });
   }
+}
+
+async function nextFreeKey(lastKey) {
+  let newKey = nextKey(lastKey);
+  while (! await module.exports.keyAvailable(newKey)) {
+    newKey = nextKey(newKey);
+  }
+  return newKey;
 }
