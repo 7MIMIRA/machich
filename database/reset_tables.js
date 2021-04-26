359e9a86298d48a8481f0ee1454709de7408da96
7MIMIRA <7mimira@gmail.com>
2021-04-26 02:36:19 -0400

    Modify lastkey init value

--- database/reset_tables.js
+++ database/reset_tables.js
@@ -2,7 +2,7 @@ const { Database, Lastkey} = require('./index.js');
 
 Database.sync({ force: true })
   .then(() => {
-    Lastkey.create({ key: 'b7cz' })
+    Lastkey.create({ key: '' })
       .then(() => {
         Database.close();
       });
